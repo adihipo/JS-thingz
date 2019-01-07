e21e9d0225706d45ea4135f4c15a5e5b2dcb00f4
Gyarmati Ádám <gyarmatiaadaam@gmail.com>
2019-01-07 23:50:34 +0100

    cicaBreedApp v3

--- cicabreedApp/static/dom.js
+++ cicabreedApp/static/dom.js
@@ -1,12 +1,32 @@
 const breedButton = document.getElementById('breed');
 const form = document.getElementsByClassName('form')[0];
 const male = document.getElementById('male');
+const downmale = document.getElementById('downmale');
+const upmale = document.getElementById('upmale');
 const female = document.getElementById('female');
+const downfemale = document.getElementById('downfemale');
+const upfemale = document.getElementById('upfemale');
 const breedAgainButton = document.getElementById('breedagain');
 const result = document.getElementsByClassName('result')[0];
 const gifs = document.getElementById('gifs');
 const resultSentence = document.getElementById('resultsentence');
 var numberOfKittens = 0;
+
+downmale.onclick = () => {
+  male.value--;
+};
+
+upmale.onclick = () => {
+  male.value++;
+};
+
+downfemale.onclick = () => {
+  female.value--;
+};
+
+upfemale.onclick = () => {
+  female.value++;
+};
 
 breedButton.onclick = () => {
   var males = parseInt(male.value);
@@ -14,7 +34,7 @@ breedButton.onclick = () => {
   var females = parseInt(female.value);
   if(!isNaN(males) && !isNaN(females)) {
     result.classList.remove('hidden');
-    if(males == 0 || females == 0) {
+    if(males <= 0 || females <= 0) {
       numberOfKittens = 0;
     } else {
       numberOfKittens = males + females * 2;
