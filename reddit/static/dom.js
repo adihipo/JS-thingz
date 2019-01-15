25e7166d7b07838018a3926acc0e04b1b365cce4
Gyarmati Ádám <gyarmatiaadaam@gmail.com>
2019-01-15 21:04:55 +0100

    feat(reddit): timer reloader

--- reddit/static/dom.js
+++ reddit/static/dom.js
@@ -1,15 +1,22 @@
 const messageContainer = document.getElementById('messages');
-
 const ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'http://localhost:8080/api/posts', true);
-ourRequest.onload = function() {
-  const ourData = JSON.parse(ourRequest.responseText);
-  renderHTML(ourData);
-};
-ourRequest.send();
+
+const ajax = () => {
+  ourRequest.open('GET', 'http://localhost:8080/api/posts', true);
+  ourRequest.onload = function() {
+    const ourData = JSON.parse(ourRequest.responseText);
+    renderHTML(ourData);
+  };
+  ourRequest.send();
+}
+
+ajax();
+setInterval(() => {
+  ajax();
+}, 5000);
 
 function renderHTML(data) {
-  console.log(data);
+  messageContainer.innerHTML = '';
   var HTMLString = "";
   for(let i = 0; i < data.length; i++) {
     HTMLString += '<div class="message">';
