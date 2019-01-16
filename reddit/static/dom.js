534c3a9e366fd6e1f088162e1fe4dd086dae9c89
Gyarmati Ádám <gyarmatiaadaam@gmail.com>
2019-01-16 22:51:38 +0100

    style(reddit): some basic styling

--- reddit/static/dom.js
+++ reddit/static/dom.js
@@ -23,9 +23,9 @@ function renderHTML(data) {
     HTMLString += '<div class="message">';
     HTMLString += '<span class="title"> title: ' + data[i].title + ' </span>';
     HTMLString += '<span class="url"> url: ' + data[i].url + ' </span>';
-    HTMLString += '<a class="upvote" id="' + data[i].id + '">+</a>';
+    HTMLString += '<a class="upvote fas fa-plus"" id="' + data[i].id + '"></a>';
     HTMLString += '<span class="score"> score: ' + data[i].score + ' </span>';
-    HTMLString += '<a class="downvote" id="' + data[i].id + '">-</a>';
+    HTMLString += '<a class="downvote fa fa-minus" id="' + data[i].id + '"></a>';
     HTMLString += '<span class="time"> posted at: ' + data[i].timestamp + ' </span>';
     HTMLString += '</div>';
   }
